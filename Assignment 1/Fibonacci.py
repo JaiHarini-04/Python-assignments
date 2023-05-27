e72cf4260479910d3687efe2275e25f302321472
JaiHarini-04 <134788738+JaiHarini-04@users.noreply.github.com>
2023-05-27 19:12:26 +0530

    Add files via upload

---
+++ Assignment 1/Fibonacci.py
@@ -0,0 +1,20 @@
+def fibonacci(n):
+    if n <= 0:
+        print("Invalid input! Please enter a positive integer.")
+        return
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        fib_sequence = [0, 1]
+        while len(fib_sequence) < n:
+            next_number = fib_sequence[-1] + fib_sequence[-2]
+            fib_sequence.append(next_number)
+        return fib_sequence
+
+
+# Example usage
+num_terms = int(input("Enter the number of Fibonacci terms you want to generate: "))
+fibonacci_sequence = fibonacci(num_terms)
+print(fibonacci_sequence)
